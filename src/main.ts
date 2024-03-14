aae3b0b9ad15f1959e6f7a00c91aaf101df180d9
Artem <temalinho@gmail.com>
2024-03-14 09:51:33 +0300

    fix address port

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:3001, http://5.188.138.67:3001'],
+    origin: ['http://localhost:3000, http://5.188.138.67:3000'],
     credentials: true,
     exposedHeaders: 'set-cookie',
   });
