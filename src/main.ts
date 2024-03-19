65cd392bcc1da2110e8d21235cf4063592009719
Artem <temalinho@gmail.com>
2024-03-19 16:35:34 +0300

    add new public ip

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://5.178.85.46:3000'],
+    origin: ['http://localhost:3000', 'http://109.71.9.170:3000'],
     credentials: true,
     exposedHeaders: 'set-cookie',
   });
